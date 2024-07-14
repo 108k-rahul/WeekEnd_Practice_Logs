1b58e54e0336fc020d0071cafdb28574abdc84c3
Rahul Kumar <108k.rahul@gmail.com>
2024-07-14 22:18:54 +0530

    Practice

---
+++ July14/armstrong.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+/*Armstrong: a no. that is equal to sum of cube of it's digits.*/
+
+int main(){
+    int n;
+    cout<<"Enter number";
+    cin>>n;
+
+    
+    return 0;
+}
---
+++ July14/leapyear.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+/*Leap Year : exactly divisible by 4 except no's ending with 00(century years).
+A century year is a leap year if it is perfectly divisible by 400*/
+
+
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+
+    if(n%400 == 0){
+        cout<<"Leap year"<< endl;
+    }else if (n%100 == 0){
+        cout<<"Not a leap year."<< endl;
+    }else if(n%4 == 0){
+        cout<<"Leap Year"<< endl;
+    }else{
+        cout<<"Not a leap year"<< endl;    
+    }    
+    return 0;
+}
